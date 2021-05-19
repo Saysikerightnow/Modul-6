120d2aeb7ff47b566d7aead7e3fb75fa8d6c9bb0
Christoffergs <christoffergs@hotmail.com>
2021-05-19 21:59:53 +0200

    cookie

--- script.js
+++ script.js
@@ -7,3 +7,16 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+});
+
+setTimeout(() => {
+   {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
