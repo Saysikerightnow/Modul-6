c67064d8ec3fcf15de4d274a896e3b15111a380a
Jonas B <74953649+DrCones@users.noreply.github.com>
2021-05-20 16:02:45 +0200

    dropdown

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Hamburger menu
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -6,4 +8,13 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+}
+
+//dropdown i menuen
+
+document.getElementById("menu").addEventListener("click", openMenu);
+
+function openMenu() {
+
+  document.getElementById("dropdown").classList.toggle("active")
 }
