8125eb14ddf5f20d8539e7902fab2959d0df2d20
Saysikerightnow <75181479+Saysikerightnow@users.noreply.github.com>
2021-05-26 23:34:19 +0200

    Udseende done

--- menu.js
+++ menu.js
@@ -3,40 +3,34 @@ function openFase(evt, fasenummer) {
 
   var i, menucontents, menulinks;
 
-  // Get all elements with class="tablinks" and remove the class "active"
+  // Get all elements with class="menulinks" and remove the class "active"
 
-  menulinks = document.getElementsByClassName("menulinks");
+  menulinks = document.getElementsByClassName("menulinks1");
   for (i = 0; i < menulinks.length; i++) {
-    menulinks[i].className = menulinks[i].className.replace(" active", "" );
+    menulinks[i].className = menulinks[i].className.replace(" active", "");
   }
 
-  // tablinks = document.getElementsByClassName("tablinks2");
-  // for (i = 0; i < tablinks.length; i++) {
-  //   tablinks[i].className = tablinks[i].className.replace(" active", "");
-  // }
+  menulinks = document.getElementsByClassName("menulinks2");
+  for (i = 0; i < menulinks.length; i++) {
+    menulinks[i].className = menulinks[i].className.replace(" active", "");
+  }
 
-  // tablinks = document.getElementsByClassName("tablinks3");
-  // for (i = 0; i < tablinks.length; i++) {
-  //   tablinks[i].className = tablinks[i].className.replace(" active", "");
-  // }
-
-  // tablinks = document.getElementsByClassName("tablinks4");
-  // for (i = 0; i < tablinks.length; i++) {
-  //   tablinks[i].className = tablinks[i].className.replace(" active", "");
-  // }
+  menulinks = document.getElementsByClassName("menulinks3");
+  for (i = 0; i < menulinks.length; i++) {
+    menulinks[i].className = menulinks[i].className.replace(" active", "");
+  }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
 
   document.getElementById(fasenummer).style.display = "block";
+  if (fasenummer == "fase1") {
+    document.getElementById("hide1").style.display = "none";
+  } else if (fasenummer == "fase2") {
+    document.getElementById("hide2").style.display = "none";
+  } else if (fasenummer == "fase3") {
+    document.getElementById("hide3").style.display = "none";
+  }
   evt.currentTarget.className += " active";
-}
-
-function myFunction() {
-  var add = document.getElementById("hide");
-  add.classList.add("test");
-
-  // ved onclick triggers funktionen som henter id'et "hide" som er på classen tab,
-  // derefter tilføjer den en class med visibility: hidden; attribute.
 }
 
 function fjernclass() {
@@ -46,10 +40,8 @@ function fjernclass() {
   var fjernfase1 = document.getElementById("fase1");
   var fjernfase2 = document.getElementById("fase2");
   var fjernfase3 = document.getElementById("fase3");
-  var fjernfase4 = document.getElementById("fase4");
 
   fjernfase1.style.display = "none";
-  // fjernfase2.style.display = "none";
-  // fjernfase3.style.display = "none";
-  // fjernfase4.style.display = "none";
+  fjernfase2.style.display = "none";
+  fjernfase3.style.display = "none";
 }
