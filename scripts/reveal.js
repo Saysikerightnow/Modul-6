23bcef2c8e1419968796d90f4e812ff7c09471fe
Saysikerightnow <75181479+Saysikerightnow@users.noreply.github.com>
2021-05-31 10:36:05 +0200

    Update reveal.js

--- scripts/reveal.js
+++ scripts/reveal.js
@@ -5,8 +5,21 @@ function reveal(){
 
   for(var i = 0; i < reveals.length; i++){
 
+    du sætter en varibel = i < 0, derefter pluser du 1 hver gang du kører funktionen.add
+
+    ligeså snart i > reveal lengt så kører den, og det samme modsat
+
+    for er et loop, den iterere ind til statement er sandt 
+
     var windowHeight = window.innerHeight;
     var revealTop = reveals[i].getBoundingClientRect().top;
+
+    revealtop som bliver til = reveals [i] array med getBoundingClientRect.add
+
+    man sætter i virkeligheden revealTop til toppen af getBoundingClientRect i reveals array. da I bliver til et array i linjen ovenover
+
+    find ud af hvad getBoundingClientRect gør.
+
     var revealPoint = 150;
 
     if(revealTop < windowHeight - revealPoint){
